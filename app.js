87db35bb6b886a91d7f111e41ae219e5dcece090
GustavoFantin <gustavofantinbarros@gmail.com>
2024-11-25 12:27:24 -0800

    exercise done

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 $(function() {
-  // YOUR CODE HERE
+  //1st exercise
+  $('#toggleButton').on('click', function () {
+    $('#myParagraph').toggle()
+  })
+
+  //2nd exercise
+  $('#colorButton').on('click', function () {
+    $('#colorDiv').css({
+      backgroundColor: 'yellow'
+    })
+  })
+
+  //3rd exercise
+  $('#addClassButton').on('click', function() {
+    $('#classDiv').addClass("randomClass")
+  })
+
+  $('#removeClassButton').on('click', function () {
+    $('#classDiv').removeClass("randomClass ")
+  })
+
+  //4th
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn()
+  })
+
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut()
+  })
+
+  //5th
+  $('#slideUpButton').on('click', function() {
+    $('#slideDiv').slideUp()
+  })
+
+  $('#slideUpButton').on('click', function() {
+    $('#slideDiv').slideDown()
+  })
 })
